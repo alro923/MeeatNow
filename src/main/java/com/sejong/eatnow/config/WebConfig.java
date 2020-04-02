c783d040ce19efd52a5f234f43dfdca6694ad165
LAPTOP-0I0SFG2M\kimhanui <cocacola2017@naver.com>
2020-04-02 22:59:31 +0900

    [ENH] cors보안 허용을 위해 WebConfig추가

---
+++ src/main/java/com/sejong/eatnow/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.sejong.eatnow.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") //모든 요청에 대해서
+                .allowedOrigins("https://meeatnow.herokuapp.com/"); //허용할 오리진들
+    }
+}
