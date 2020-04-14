14db719f9183866ab69470a34ebf64d997eac1ef
LAPTOP-0I0SFG2M\kimhanui <cocacola2017@naver.com>
2020-04-15 04:28:56 +0900

    [CLN] cors - springconfig 추가(permitAll)

--- src/main/java/com/sejong/eatnow/config/SecurityConfig.java
+++ src/main/java/com/sejong/eatnow/config/SecurityConfig.java
@@ -2,11 +2,23 @@ package com.sejong.eatnow.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsUtils;
 
+@EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    protected void configure(HttpSecurity http) {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers("/**").permitAll();
+
 
     }
+
 }
