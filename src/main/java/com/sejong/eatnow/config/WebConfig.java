41d6b76df42680dc80d29584156c17db18dfad5e
LAPTOP-0I0SFG2M\kimhanui <cocacola2017@naver.com>
2020-04-15 03:57:27 +0900

    [CLN] config 상위 클래스 수정 및 properties 수정

--- src/main/java/com/sejong/eatnow/config/WebConfig.java
+++ src/main/java/com/sejong/eatnow/config/WebConfig.java
@@ -2,29 +2,22 @@ package com.sejong.eatnow.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//EnableWebMVC쓰지 말것.
 @Configuration
-public class WebConfig extends WebSecurityConfigurerAdapter {
-
+public class WebConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 요청에 대해서
-                .allowedOrigins("https://localhost:8080/") //허용할 오리진들
+                .allowedOrigins("http://localhost:8080","https://localhost:8080") //허용할 오리진들
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
-
+                        HttpMethod.DELETE.name())
+                .allowCredentials(false);
     }
 }
